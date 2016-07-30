843479fdb90f45f5b9b420c6848a4d109ae7637c
jonnSmith <eugenpushkaroff@gmail.com>
2016-07-30 21:21:27 +0300

    fixThemeOptions

--- wpUtils/wpThemeoptions.php
+++ wpUtils/wpThemeoptions.php
@@ -16,29 +16,29 @@ class wpThemeoptions {
 
         $themeOptions = array();
 
-        foreach($this->options as $option) {
-            if(isset($option["id"]) && $option["id"] != '' && $option["id"] != $shortname.'_code')
-                $themeOptions[$option["id"]] = "";
+        if(!empty($this->options)) {
+            foreach ($this->options as $option) {
+                if (isset($option["id"]) && $option["id"] != '' && $option["id"] != $shortname . '_code')
+                    $themeOptions[$option["id"]] = "";
+            }
+            $themeKeys = '"';
+            $themeKeys .= join('","', array_keys($themeOptions));
+            $themeKeys .= '"';
+            $optionValues = $wpdb->get_results('SELECT option_name, option_value FROM wp_options WHERE option_name IN (' . $themeKeys . ')');
+
+
+            foreach ($optionValues as $optionValue) {
+                $themeOptions[$optionValue->option_name] = $optionValue->option_value;
+            }
+
+            $themeOptions['feed'] = get_feed_link();
+            $themeOptions['site_name'] = get_bloginfo('name');
+            $themeOptions['site_link'] = esc_url(home_url('/'));
+
+            wp_reset_query();
+            $wpdb->flush();
+
+            return $themeOptions;
         }
-        $themeKeys = '"';
-        $themeKeys .= join('","',array_keys($themeOptions));
-        $themeKeys .= '"';
-        $optionValues = $wpdb->get_results('SELECT option_name, option_value FROM wp_options WHERE option_name IN ('.$themeKeys.')');
-
-
-        foreach($optionValues as $optionValue) {
-            $themeOptions[$optionValue->option_name] = $optionValue->option_value;
-        }
-
-        $themeOptions['feed'] = get_feed_link();
-        $themeOptions['site_name'] = get_bloginfo('name');
-        $themeOptions['site_link'] =  esc_url(home_url( '/' ));
-
-        wp_reset_query();
-        $wpdb->flush();
-
-        return $themeOptions;
-
     }
-
 }
